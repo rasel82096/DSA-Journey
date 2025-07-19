3500c5388c9b5a649fa3ed9845568af1f1e16a3c
Rasel Ahammed <155523780+rasel82096@users.noreply.github.com>
2025-07-19 22:06:38 +0600

    Add binary search implementation
    Implement binary search for descending order array"

--- Arrays/Binary Search/1.cpp
+++ Arrays/Binary Search/1.cpp
@@ -1,7 +1,36 @@
+//binary search for decreasing order Array
 #include<bits/stdc++.h>
 using namespace std;
+int binary_search(vector<int> &arr,int target){
+  int sizee=arr.size();
+  int left=0,right=sizee-1;
+  while(left<=right){
+    int mid=left+(right-left)/2;
+    if(arr[mid]==target){
+      return mid;
+    }
+    
+    else if(target>arr[mid]){
+      right=mid-1;
+    }
+    else if(target<arr[mid]){
+      left=mid+1;
+    }
+  }
+
+  return -1;
+
+}
 int main()
-{  cout<<"HI Hello";
-  cout<<"Change Happend";
-  return 0;
+{  
+  int n;
+  cin>>n;
+  vector<int>arr(n);
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+ cout<<"Enter Target: ";
+ int target;
+ cin>>target;
+  cout<<binary_search(arr,target);
 }
